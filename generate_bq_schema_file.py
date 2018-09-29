e10a760b23d62a1cc0d6b9c3f800e4479a29793d
bruecearctor <iambrucearctor@gmail.com>
2018-09-28 22:54:06 -0700

    fileoutput

---
+++ generate_bq_schema_file.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import json
+
+## NOT NULLABLE -- WHAT IS THIS IN BQ???
+filename = sys.argv[1]
+
+bq_schema_json = {}
+
+## HOW TO HANDLE escape characters and/or does single-vs-double quotes matter??
+
+bq_schema = []
+required = set()
+data_types_map = {
+    'string': "STRING"
+    ## Int, etc?
+    ## DOESN'T INCLUDE OBJECT!
+}
+
+def lowerandunderscore(columnname):
+	return columnname.replace(' ', '_').lower() #if type(columnname) == string() else "oh"
+
+#read file
+with open(filename, 'r') as f:
+    genson_schema = json.load(f)
+
+## ASSUMES REQUIRED ARE ONLY TOP LEVEL
+for column in genson_schema['required']:
+    required.add(column)
+
+# Assumes max nested of 1
+for column in genson_schema['properties']:
+    nested = False
+    bq_col = {}
+    col_type =  genson_schema['properties'][column]['type']
+    if col_type in data_types_map.keys():
+        bq_col['name'] = lowerandunderscore(column)
+        bq_col['type'] = data_types_map[col_type]
+        bq_col['mode'] = "NOT NULLABLE" if column in required else "NULLABLE"
+    elif col_type == 'object':
+        nested = True
+        if 'properties' in genson_schema['properties'][column].keys():
+            for nested_col in genson_schema['properties'][column]['properties']:
+                bq_col = {}
+                bq_col['name'] = lowerandunderscore("{}_{}".format(column,nested_col))
+                bq_col['type'] = data_types_map[genson_schema['properties'][column]['properties'][nested_col]['type']]
+                bq_col['mode'] = "NULLABLE"
+                bq_schema.append(bq_col)
+    else:
+        print("not string or record type")
+        print("column is: {}".format(column))
+        exit(1)
+
+    if nested == False:
+        bq_schema.append(bq_col)
+
+
+with open('bq_schema.json', 'w') as f:
+    bq_schema_json["BigQuery Schema"] = bq_schema
+    f.write(json.dumps(bq_schema_json))
+
+
+
+
---
+++ js_template_bq_schema.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import json
+
+## NOT NULLABLE -- WHAT IS THIS IN BQ???
+filename = sys.argv[1]
+
+js_start = [
+"function transform(line) {",
+"var values = JSON.parse(line);",
+"var obj = new Object();"
+]
+
+js_end = [
+"var jsonString = JSON.stringify(obj);",
+"return jsonString;",
+"}"
+]
+
+list_of_jsonlines = []
+
+def lowerandunderscore(columnname):
+    return columnname.replace(' ', '_').lower() #if type(columnname) == string() else "oh"
+
+#read file
+with open(filename, 'r') as f:
+    genson_schema = json.load(f)
+
+# Assumes max nested of 1
+for column in genson_schema['properties']:
+    nested = False
+    col_type =  genson_schema['properties'][column]['type']
+    if col_type == 'object':
+        nested = True
+        if 'properties' in genson_schema['properties'][column].keys():
+            for nested_col in genson_schema['properties'][column]['properties']:
+                json_builder = "obj.{} = values['{}']['{}']".format(lowerandunderscore("{}_{}".format(column,nested_col)),column,nested_col)
+                list_of_jsonlines.append(json_builder)
+    else:
+        json_builder = "obj.{} = values['{}']".format(lowerandunderscore(column),column)
+
+    if nested == False:
+        list_of_jsonlines.append(json_builder)
+
+
+full_json_file = js_start + list_of_jsonlines + js_end
+
+with open('output.js', 'w') as f:
+    for line in full_json_file:
+        f.write("{}\n".format(line.strip()))
